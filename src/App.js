1686a77f0c016f7bcf38b7e50d627fb6582dbc3f
Khalil011 <76681704+Khalil011@users.noreply.github.com>
2021-07-15 15:08:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="header"></div>
+      <div className="flex-container">
+        <div className="left">
+          <div className="lflex"></div>
+          <div className="lflex"></div>
+          <div className="lflex"></div>
+          <div className="lflex"></div>
+        </div>
+        <div className="right"></div>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
+}
